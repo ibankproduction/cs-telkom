75489f02693962ec7a0d6d6f9eb15c1b516f3a70
muhammad iqbal <m.iqbal.nasutioon@gmail.com>
2023-01-10 08:12:54 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost","root","");
-mysql_select_db("drasticdata");
+mysql_connect("localhost","id20125985_root","tL[t1bXc7rNh=U8h");
+mysql_select_db("id20125985_drasticdata");
 ?>
